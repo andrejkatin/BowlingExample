cdf1417b5dfac5f525d56b02d4c2a023bd25ff23
Andrej Katin <katin.andrej96@gmail.com>
2021-10-25 16:57:44 +0200

    Add project files.

---
+++ BowlingExample/Program.cs
@@ -0,0 +1,14 @@
+﻿using BowlingExampleTemplate.Bowling;
+using System;
+
+namespace BowlingExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lets play a bowling game...");
+            BowlingLogic.bowlingGameWithRNG();
+        }
+    }
+}
